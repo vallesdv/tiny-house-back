01758f073f60fd30fd4a0d76626fe40358bdc168
d-valles <davalles27@gmail.com>
2020-04-26 23:25:00 -0600

    Base server

---
+++ server/src/graphql/resolvers.ts
@@ -0,0 +1,20 @@
+import { IResolvers } from 'apollo-server-express';
+import { listings } from '../listing';
+
+export const resolvers: IResolvers = {
+    Query: {
+      listing: () => {
+        return listings;
+      }
+    },
+    Mutation: {
+      deleteListing: (_root: undefined, { id }: { id: string}) => {
+        for (let i = 0; i < listings.length; i++) {
+          if (listings[i].id === id) {
+              return listings.splice(i, 1)[0];
+          }
+        }
+        throw new Error('failed to delete listing');
+      }
+    }
+};
---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { typeDefs, resolvers } from './graphql';
+
+const app = express();
+const port = 9000;
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({app, path: '/api'});
+
+app.listen(port);
+
+console.log(`[myMagiPort]: http://localhost:${port}`);
