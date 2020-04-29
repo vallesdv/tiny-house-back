d43426ce08e5d869a7165e312ab5a99051d02c77
d-valles <davalles27@gmail.com>
2020-04-28 19:19:52 -0600

    Mongosetup and env variables, first iteration

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,25 @@
-import express from 'express';
+require('dotenv').config();
+
+import express, { Application } from 'express';
 import { ApolloServer } from 'apollo-server-express';
+import { connectDatabase } from "./database";
 import { typeDefs, resolvers } from './graphql';
 
-const app = express();
-const port = 9000;
+const mount = async (app: Application) => {
+    const db = await connectDatabase();
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        context: () => ({ db })
+    });
+    server.applyMiddleware({app, path: '/api'});
+    
+    app.listen(process.env.PORT);
+    
+    console.log(`[myMagiPort]: http://localhost:${process.env.PORT}`);
 
-const server = new ApolloServer({ typeDefs, resolvers });
-server.applyMiddleware({app, path: '/api'});
+    const listings = await db.listings.find({}).toArray();
+    console.log(listings);
+}
 
-app.listen(port);
-
-console.log(`[myMagiPort]: http://localhost:${port}`);
+mount(express());
