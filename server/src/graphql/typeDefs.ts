7722ed6629bb01afa2d7b422c0fde1df11c53993
d-valles <davalles27@gmail.com>
2020-07-28 11:33:00 -0600

    People api

--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -1,23 +1,11 @@
 import { gql } from 'apollo-server-express';
 
 export const typeDefs = gql`
-	type Listing {
-		id: ID!
-		title: String!
-		image: String!
-		address: String!
-		price: Int!
-		numOfGuests: Int!
-		numOfBeds: Int!
-		numOfBaths: Int!
-		rating: Int!
-  }
-  
-  type Mutation {
-    deleteListing(id: ID!): Listing!
-  }
-  
-  type Query {
-    listing: [Listing!]!
-  }
+    type Query {
+        authUrl: String!
+    }
+    type Mutation {
+        logIn: String!
+        logOut: String!
+    }
 `;
